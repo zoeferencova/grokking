c1480007572d6b9d99662f1836831aeed650e340
zoeferencova <zoeferencova@gmail.com>
2021-02-24 14:00:53 -0500

    added middle of linked list

---
+++ fast-and-slow-pointers/middle-of-linked-list.js
@@ -0,0 +1,36 @@
+// Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+// If the total number of nodes in the LinkedList is even, return the second middle node.
+
+// Solution
+class Node {
+    constructor(value, next=null){
+        this.value = value;
+        this.next = next;
+    }
+  }
+  
+const find_middle_of_linked_list = function(head) {
+    let slow = head, fast = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
+
+head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(5)
+
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
+
+head.next.next.next.next.next = new Node(6)
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
+
+head.next.next.next.next.next.next = new Node(7)
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
+
+// Time complexity: O(N)
+// Space complexity: O(1)
